186b0cc11d4233226c23769fd77f52c95d7bd737
Mark Lakewood <underplank@gmail.com>
2013-09-02 11:05:36 -0700

    made procfile work

--- food_trucks/run_server.py
+++ food_trucks/run_server.py
@@ -2,10 +2,13 @@
 from food_trucks import my_app
 from food_trucks.app import define_urls
 
-
-if __name__ == '__main__':
+def run_server():
     from food_trucks.db import get_engine, get_db_session
     engine = get_engine('sqlite:///food_truck.sql')
     my_app.config['DB_SESSION'] = get_db_session(engine)
     define_urls(my_app)
-    my_app.run(debug=True)
+    return my_app
+
+if __name__ == '__main__':
+    app = run_server()
+    app.run()
